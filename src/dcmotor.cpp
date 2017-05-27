e1ec5bfb41221c6396d3b2b6f960eca133f88a18
Dean <deanwong@126.com>
2017-05-27 16:42:24 +0800

    change comment

--- src/dcmotor.cpp
+++ src/dcmotor.cpp
@@ -11,34 +11,34 @@ DcMotor::DcMotor(int pin1, int pin2, int pwm)
     pinMode(pin2, OUTPUT); //输出第二个引脚
 
     /*
-  Pins 5 and 6: controlled by Timer 0 in fast PWM mode (cycle length = 256)
+      Pins 5 and 6: controlled by Timer 0 in fast PWM mode (cycle length = 256)
 
-  Setting   Divisor   Frequency
-  0x01    1     62500
-  0x02      8     7812.5
-  0x03      64    976.5625   <--DEFAULT
-  0x04    256     244.140625
-  0x05    1024    61.03515625
+      Setting   Divisor   Frequency
+      0x01    1     62500
+      0x02      8     7812.5
+      0x03      64    976.5625   <--DEFAULT
+      0x04    256     244.140625
+      0x05    1024    61.03515625
 
-  TCCR0B = (TCCR0B & 0b11111000) | <setting>;
-  */
+      TCCR0B = (TCCR0B & 0b11111000) | <setting>;
+    */
     // TCCR0B = (TCCR0B & 0b11111000) | 5;
     /*
-  Pins 9 and 10: controlled by timer 1 in phase-correct PWM mode (cycle length =
-  510)
+      Pins 9 and 10: controlled by timer 1 in phase-correct PWM mode (cycle length =
+      510)
 
-  Setting   Divisor   Frequency
-  0x01    1     31372.55
-  0x02    8     3921.16
-  0x03      64    490.20   <--DEFAULT
-  0x04      256     122.55
-  0x05    1024    30.64
+      Setting   Divisor   Frequency
+      0x01    1     31372.55
+      0x02    8     3921.16
+      0x03      64    490.20   <--DEFAULT
+      0x04      256     122.55
+      0x05    1024    30.64
 
-  TCCR1B = (TCCR1B & 0b11111000) | <setting>;
+      TCCR1B = (TCCR1B & 0b11111000) | <setting>;
 
-  Pins 11 and 3:  controlled by timer 2.
-    TCCR2B = (TCCR2B & 0b11111000) | <setting>;
-  */
+      Pins 11 and 3:  controlled by timer 2.
+        TCCR2B = (TCCR2B & 0b11111000) | <setting>;
+    */
     // TCCR1B = (TCCR1B & 0b11111000) | 5;
 }
 
